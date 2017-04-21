d4404c8f52e4c8403f0781500448d541c1a3127a
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-04-21 19:12:23 -0300

    Finish routing

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,5 +1,5 @@
 import 'rxjs/add/operator/switchMap';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params }   from '@angular/router';
 import { Location }                 from '@angular/common';
 import { Hero } from './hero';
@@ -7,7 +7,8 @@ import { HeroService } from './hero.service';
 
 @Component({
   selector: 'hero-detail',
-  templateUrl: './hero-detail.component.html'
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
 })
 
 export class HeroDetailComponent implements OnInit {
@@ -17,7 +18,7 @@ export class HeroDetailComponent implements OnInit {
     private location: Location
   ) {}
 
-  @Input() hero: Hero;
+  hero: Hero;
 
   ngOnInit(): void {
     this.route.params
