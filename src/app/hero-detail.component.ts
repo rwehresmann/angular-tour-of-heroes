0c98edb87e0a6434bb0a45364584e4199ec8389c
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-04-21 15:31:56 -0300

    Implement 'goBack'

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -7,16 +7,7 @@ import { HeroService } from './hero.service';
 
 @Component({
   selector: 'hero-detail',
-  template: `
-    <div *ngIf="hero">
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name">
-      </div>
-    </div>
-    `
+  templateUrl: './hero-detail.component.html'
 })
 
 export class HeroDetailComponent implements OnInit {
@@ -32,5 +23,9 @@ export class HeroDetailComponent implements OnInit {
     this.route.params
         .switchMap((params: Params) => this.heroService.getHero(+params['id']))
         .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 }
